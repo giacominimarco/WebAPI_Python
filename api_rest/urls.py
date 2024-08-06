e78f02775dcc8fa4e94c40d255f15d7697b71ba8
Marco Antonio Giacomini <giacomini_m.a@hotmail.com>
2024-08-06 15:57:40 -0300

    adjusted delete method

--- api_rest/urls.py
+++ api_rest/urls.py
@@ -3,13 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('home/', views.home, name='home'),
     path('users/', views.users, name='users'),
     path('register/', views.register, name='register'),
     path('add/', views.add, name='add'),
-    path('delete/<str:login>/', views.delete, name='delete'),
-
-    # path('', views.get_users, name='get_all_users'),
-    # path('user/<str:login>', views.get_by_login, name='get_by_login'),
-    # path('data/', views.user_manager, name='user_manager'),
+    path('users/delete/<str:username>', views.delete, name='delete'),
 ]
--- api_rest/views.py
+++ api_rest/views.py
@@ -1,126 +1,59 @@
 from django.shortcuts import redirect, render
-# from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login as login_django, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User as User_Django
 
-# from rest_framework.decorators import api_view
-# from rest_framework.response import Response
-# from rest_framework import status
-# from .serializers import UserSerializer
-from .models import User
+def login(request):
+    if request.method == "GET":
+        return render(request,'login.html')
+    else:
+        login = request.POST.get('login')
+        password = request.POST.get('password')
+        user = authenticate(username=login, password=password)
+        if user is not None:
+            login_django(request, user)
+            return render(request,'users/home.html')
+        else:
+            return HttpResponse('Login or password invalid')
+        
+def logout_view(request):
+    logout(request)
+    return redirect('login')
 
-import json
-
+@login_required(login_url="/auth/login/")
 def home(request):
     return render(request,'users/home.html')
 
+
+@login_required(login_url="/auth/login/")
 def users(request):
     users = {
-        'users': User.objects.all()
+        'users': User_Django.objects.all()
     }
     return render(request,'users/users.html',users)
+
 
 def register(request):
     return render(request,'users/register.html')
 
+
 def add(request):
-    new_user = User()
-    new_user.user_login = request.POST.get('login')
-    new_user.user_password = request.POST.get('password')
+    new_login = request.POST.get('login')
+    new_password = request.POST.get('password')
+    hasUser = User_Django.objects.filter(username=new_login).first()
+    if hasUser:
+        return render(request,'users/register.html')
+    new_user = User_Django.objects.create_user(username=new_login, password=new_password)
     new_user.save()
-    users = {
-        'users': User.objects.all()
-    }
     return render(request,'users/register.html')
 
-def delete(request, login):
-    user_delete = User.objects.get(pk=login)
+
+@login_required(login_url="/auth/login/")
+def delete(request, username):
+    user_delete = User_Django.objects.get(username=username)
     user_delete.delete()
-    return render('users/users.html')
-    # return render(request,'users/users.html',users)
-
-# @api_view(['GET'])
-# def get_users(request):
-    
-#     if request.method == 'GET':
-#         users = User.objects.all()
-#         serializer = UserSerializer(users, many=True)
-#         return Response(serializer.data)
-#     return Response(status=status.HTTP_400_BAD_REQUEST)
-
-# @api_view(['GET','PUT'])
-# def get_by_login(request, login):
-    
-#     try:
-#         user = User.objects.get(pk=login)
-#     except:
-#         return Response(status=status.HTTP_404_NOT_FOUND)
-    
-#     if request.method == 'GET':
-#         serializer = UserSerializer(user)
-#         return Response(serializer.data)
-    
-#     if request.method == 'PUT':
-#         serializer = UserSerializer(user, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
-    
-#     return Response(status=status.HTTP_400_BAD_REQUEST)
-
-
-# @api_view(['GET','POST','PUT','DELETE'])
-# def user_manager(request):
-    
-# # GET USER
-#     if request.method == 'GET':
-#         try:
-#             user_login = request.GET['login']
-            
-#             try:
-#                 user = User.objects.get(pk=user_login)
-#             except:
-#                 return Response(status=status.HTTP_404_NOT_FOUND)
-        
-#             serializer = UserSerializer(user)
-#             return Response(serializer.data)
-        
-#         except:
-#             return Response(status=status.HTTP_400_BAD_REQUEST)
-
-# # SAVE USER
-#     if request.method == 'POST':
-#         new_user = request.data
-#         serializer = UserSerializer(data=new_user)
-        
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
-#         return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
-        
-# # EDIT USER
-#     if request.method == 'PUT':
-#         login = request.data['user_login']
-#         update_user = User.objects.get(pk=login)
-
-#         try:
-#             update_user = User.objects.get(pk=login)
-#         except:
-#             return Response(status=status.HTTP_404_NOT_FOUND)
-        
-#         print(f'Data = {request.data}')
-#         serializer = UserSerializer(update_user, data=request.data)
-
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_202_CREATED)
-        
-#         return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
-
-# # DELETE USER
-#     if request.method == 'DELETE':
-#         try:
-#             user_delete = User.objects.get(pk=request.data['user_login'])
-#             user_delete.delete()
-#             return Response(status=status.HTTP_202_ACCEPTED)
-#         except:
-#             return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+    users = {
+        'users': User_Django.objects.all()
+    }
+    return render(request,'users/users.html',users)
